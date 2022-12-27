faa9c72df721df516517cbe691fc525a0c955151
Tim Bedard <tpbedard@gmail.com>
2022-12-27 13:54:54 -0600

    add grave to space layer on q2

--- keyboards/keychron/q2/rev_0111/keymaps/timbedard/keymap.c
+++ keyboards/keychron/q2/rev_0111/keymaps/timbedard/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_____, KC_____, KC_____,                            KC_____,                            KC_____, KC_____,  KC_____, S(KC_TAB), LSG(KC_DOWN), KC_TAB),
 
     [_RAISE] = LAYOUT_ansi_67(  // Hold Space
-        KC_____, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_____,          KC_____,
+        KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,    KC_____,          KC_____,
         KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____,  KC_____,   KC_____,          KC_____,
         KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_____, KC_____,             KC_____,          KC_____,
         KC_____,          KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____, KC_____,             KC_____, LSG(KC_UP),
